84f373cfa0a8e7b44819b25c89ad2bdabd5c9332
Psevdon1m <Valentine.oleynik@gmail.com>
2023-07-25 19:34:52 +0300

    pure recursion approach is used to get odd values

--- 02_Recursion/helperMethodPattern.js
+++ 02_Recursion/helperMethodPattern.js
@@ -10,18 +10,48 @@
  *   Explanation: The function collects all odd integers from the input array and returns them in a new array.
  */
 
+// function collectOddValues(arr) {
+//   let result = [];
+
+//   function helper(helperInput) {
+//     if (!helperInput.length) return;
+//     if (helperInput[0] % 2 !== 0) {
+//       result.push(helperInput[0]);
+//     }
+//     helper(helperInput.slice(1));
+//   }
+//   helper(arr);
+//   return result;
+// }
+
+/**
+ * ALTERNATIVE WAY USING PURE RECURSION
+ * @title Collect Odd Values
+ * @dev This function takes an array of integers as input and recursively collects
+ *      all the odd values from the array into a new array.
+ * @param arr {number[]} - The input array of integers.
+ * @return {number[]} - An array containing all the odd values from the input array.
+ *
+ * @notice The function uses recursion to traverse the input array and collects
+ *         odd values into a new array called "result".
+ *         If the input array is empty, the function returns an empty array.
+ *         If the first element of the input array is odd, it is added to the "result" array.
+ *         The function then calls itself recursively with the rest of the array (arr.slice(1)),
+ *         and the result is concatenated with the "result" array to accumulate the final
+ *         array of odd values.
+ *
+ * @example
+ * // returns [1, 3, 5, 7, 9]
+ * collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+ */
+
 function collectOddValues(arr) {
   let result = [];
-
-  function helper(helperInput) {
-    if (!helperInput.length) return;
-    if (helperInput[0] % 2 !== 0) {
-      result.push(helperInput[0]);
-    }
-    helper(helperInput.slice(1));
+  if (!arr.length) return result;
+  if (arr[0] % 2 !== 0) {
+    result.push(arr[0]);
   }
-  helper(arr);
-  return result;
+  return result.concat(collectOddValues(arr.slice(1)));
 }
 
 console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
